ae0f31a955b0fbbf0c54bbd9fc1fa8995625d850
lazarulian <69273628+lazarulian@users.noreply.github.com>
2022-05-01 18:06:15 -0700

    Problem 2

---
+++ lab4/lab4.R
@@ -0,0 +1,12 @@
+## Lab Four R Script for Ease
+
+library(xtable)
+library(tidyverse)
+library(readxl)
+library(ggthemes)
+
+
+xdf <- read_tsv("/Users/apurvashah/Documents/GitHub/stats10/lab4/NHL_20202021_teamGame.tsv", col_names = TRUE)
+head(xdf)
+
+
