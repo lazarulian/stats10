5ab8477c4371450871840673f821a39f95a9ebdc
lazarulian <69273628+lazarulian@users.noreply.github.com>
2022-05-08 21:53:34 -0700

    Update Homework5

--- lab5/testenv.R
+++ lab5/testenv.R
@@ -7,30 +7,36 @@ library(ggthemes)
 
 
 nhldf <- read.table("/Users/apurvashah/Documents/GitHub/stats10/lab5/NHL_20202021_teamGame.tsv", sep="\t", header=TRUE)
+head(nhldf)
 # Penalty Minutes Team
 penalty_minutes <- nhldf[ , "team_pim"]
 penal_highlow <- c("low", "high")[ as.integer(penalty_minutes >= 8) + 1 ]
 team_table <- table(penal_highlow)[ c("low", "high") ]
+actual_high <- team_table[2]
 team_table <- team_table / sum(team_table)
 ## Penalty Minutes Other Team
-
-other_penalty <- nhldf[ cross_ndx, "shots"]
-other_penalty_min <- c("low", "high")[ as.integer(other_penalty >= 33) + 1 ]
+other_penalty <- nhldf[ , "penaltyMinutes"]
+other_penalty_min <- c("low", "high")[ as.integer(other_penalty >= 8) + 1 ]
 other_min <- table(other_penalty_min)[ c("low", "high") ]
+actual_low <- other_min[1]
 other_min <- other_min / sum(other_min)
 
-
 totalAandB <- sum(penal_highlow == "high" & other_penalty_min == "low")
-totalAandB
-propAandB <- totalAandB/length(bruns)
+propAandB <- totalAandB/length(penal_highlow)
 
 xmaskB <- other_penalty_min == "low"
 xcat_bruns_givenB <- penal_highlow[xmaskB]
 propAgivenB <- sum(xcat_bruns_givenB == "high") / length(xcat_bruns_givenB)
 
 
+###################
+## Answers
+###################
 
-paste("High Team Penalty Minutes: Proportion of G: ", team_table[2], sep = " ")
-paste("Low Opponent Penalty Minutes: Proportion of H: ", other_min[1], sep = " ")
-paste("c: Proportion of G and H: ", propAandB, sep = " ")
-paste("d: Proportion of G given H: ", propAgivenB, sep = " ")
+paste("a: Proportion of C: ", team_table[2], sep = " ")
+paste("b: Proportion of D: ", team_table[1], sep = " ")
+paste("c: Proportion of C and D: ", propAandB, sep = " ")
+paste("d: Proportion of C given D: ", propAgivenB, sep = " ")
+
+paste("High team penalty minutes", actual_high, sep = " ")
+paste("Low Opponent team penalty minutes", actual_low, sep = " ")
